a6b261f65be09a648ebe2fb10968e8e4795ae7a9
SamuelMiddendorp <31695207+SamuelMiddendorp@users.noreply.github.com>
2021-06-23 17:38:28 +0200

    Added pyd modle

--- verlet_win32.py
+++ verlet_win32.py
@@ -4,11 +4,12 @@ import math
 import random
 import os
 import sys
+import colorama
 from physicsobjects import *
 GRAVITY = 0.1
 FRICTION = 0.99
 BOUNCE = 0.01
-WIND = 1
+WIND = 0.0
 
 dims = (320,100)
 def get_distance(p1, p2): 
@@ -44,7 +45,7 @@ def draw_line(p1: Point, p2: Point, world: list):
                 y = y2
             for x in range(round(x1), round(x2)):
                 #window.addstr(round(y),round(x), " ", cs.color_pair(1)) 
-                world[round(y) * dims[0] + x] = "\u2588"
+                world[round(y) * dims[0] + x] =  "\u2588"
                 bh_offset += delt
                 if bh_offset >= bh_threshold:
                     y += incr
@@ -67,8 +68,8 @@ def update_points(points: list):
     for point in points:
         if not point.fixed:
             old_point = copy.copy(point)
-            point.x += (point.x - point.old_x) * FRICTION * WIND
-            point.y += (point.y - point.old_y) * FRICTION * WIND
+            point.x += (point.x - point.old_x) * FRICTION + WIND
+            point.y += (point.y - point.old_y) * FRICTION 
             point.y += GRAVITY
             point.old_x = old_point.x
             point.old_y = old_point.y
@@ -80,12 +81,9 @@ def update_links(links: list):
             dy = link.p2.y - link.p1.y
             dist = math.sqrt(dx * dx + dy * dy)
             diff = link.len - dist
-            try:
-                percent = diff / dist / 2
-            except:
-                percent = 0
-            offset_x = dx * percent
-            offset_y = dy * percent
+            percent = diff / dist / 2
+            offset_x = dx * percent 
+            offset_y = dy * percent 
             if not link.p1.fixed:
                 link.p1.x -= offset_x
                 link.p1.y -= offset_y
@@ -138,10 +136,11 @@ def setup_screen(dims: tuple):
     myConsole.SetConsoleWindowInfo(Absolute=True,ConsoleWindow = win32console.PySMALL_RECTType(0,0,1,1))        
 
     myConsole.SetConsoleScreenBufferSize(win32console.PyCOORDType(dims[0],dims[1]))
-
+   
     myConsole.SetConsoleWindowInfo(Absolute=True,ConsoleWindow = win32console.PySMALL_RECTType(0,0,dims[0]-1,dims[1]-1))  
     #myConsole.SetConsoleDisplayMode(Flags=win32console.CONSOLE_FULLSCREEN, NewScreenBufferDimensions = win32console.PyCOORDType(200,50))                                      
     myConsole.SetConsoleActiveScreenBuffer()
+    
     myConsole.SetConsoleCursorInfo(Size=1,Visible=0)
     return myConsole
 def setup_empty_world(total_size):
@@ -153,65 +152,78 @@ def clear_world(world, size):
     for x in range(len(world)):
         world[x] = " "
 def main():
+    global WIND
     total_size = dims[0] * dims[1]
     console = setup_screen(dims)
-    # cloth_points = generate_points(8,24,10)
-    # cloth_links = generate_links(cloth_points,8,24)
-    cloth_points = [Point(0,30,0,30,True),
-                    Point(10,30,10,30,False),
-                    Point(20,30,20,30,False),
-                    Point(30,30,30,30,False),
-                    Point(40,30,40,30,False),
-                    Point(30,10,30,10,False),
-                    Point(40,10,40,10,False),
-                    ]
-    cloth_links = [Link(cloth_points[0],cloth_points[1], get_distance(cloth_points[0],cloth_points[1])),
-                   Link(cloth_points[1],cloth_points[2], get_distance(cloth_points[1],cloth_points[2])),
-                   Link(cloth_points[2],cloth_points[3], get_distance(cloth_points[2],cloth_points[3])),
-                   Link(cloth_points[3],cloth_points[4], get_distance(cloth_points[3],cloth_points[4])),
-                   Link(cloth_points[4],cloth_points[5], get_distance(cloth_points[4],cloth_points[5])),
-                   Link(cloth_points[5],cloth_points[6], get_distance(cloth_points[5],cloth_points[6])),
-                   Link(cloth_points[4],cloth_points[6], get_distance(cloth_points[4],cloth_points[6])),
-                   Link(cloth_points[3],cloth_points[5], get_distance(cloth_points[4],cloth_points[6])),
-                   ]
+    colorama.init()
+    cloth_points = generate_points(10,30,7)
+    cloth_links = generate_links(cloth_points,10,30)
+    # cloth_points = [Point(0,30,0,30,True),
+    #                 Point(10,30,10,30,False),
+    #                 Point(20,30,20,30,False),
+    #                 Point(30,30,30,30,False),
+    #                 Point(40,30,40,30,False),
+    #                 Point(30,10,30,10,False),
+    #                 Point(40,10,40,10,False),
+    #                 ]
+    # cloth_links = [Link(cloth_points[0],cloth_points[1], get_distance(cloth_points[0],cloth_points[1])),
+    #                Link(cloth_points[1],cloth_points[2], get_distance(cloth_points[1],cloth_points[2])),
+    #                Link(cloth_points[2],cloth_points[3], get_distance(cloth_points[2],cloth_points[3])),
+    #                Link(cloth_points[3],cloth_points[4], get_distance(cloth_points[3],cloth_points[4])),
+    #                Link(cloth_points[4],cloth_points[5], get_distance(cloth_points[4],cloth_points[5])),
+    #                Link(cloth_points[5],cloth_points[6], get_distance(cloth_points[5],cloth_points[6])),
+    #                Link(cloth_points[4],cloth_points[6], get_distance(cloth_points[4],cloth_points[6])),
+    #                Link(cloth_points[3],cloth_points[5], get_distance(cloth_points[4],cloth_points[6])),
+    #                ]
     world = setup_empty_world(total_size)
     force_timer = 0
-    fps = 30
+    fps = 120
+    frame_time = 1000/fps
     amount_cloth_points = len(cloth_points)
     force_interval = 2000
+    ms = 1000
+    calc = 0
     while True: 
+        if 1000/ms > fps:
+            calc = (frame_time - ms)
+            time.sleep(calc * 0.001)
         begin_time = time.time()
         clear_world(world,total_size)
         update_points(cloth_points)
-        update_links(cloth_links)
-        constrain_points(cloth_points, world, (dims[0] - 1, dims[1] -1)) 
+        for _ in range(1):
+            update_links(cloth_links)
+            constrain_points(cloth_points, world, (dims[0] - 1, dims[1] -1)) 
         if force_timer > force_interval:
-            cloth_points[5].x += 2
-            if force_timer > force_interval * 1.5:
+            # cloth_links[11] = None
+            # cloth_links[12] = None
+            # cloth_links[13] = None
+            # cloth_links[14] = None
+            # cloth_links[15] = None
+            # cloth_links[16] = None
+            # cloth_links[17] = None
+            # cloth_links[18] = None
+            # cloth_links[19] = None
+            # cloth_links[22] = None
+            # cloth_links[23] = None
+            # cloth_links[24] = None
+            # cloth_links[25] = None
+            WIND = 0.05
+            if force_timer > force_interval * 1.4:
+                WIND = 0.0
                 force_timer = 0
-            # cloth_points[amount_cloth_points-1].x += 50
-            # cloth_points[amount_cloth_points-1].old_x += 50
-        #     cloth_links[11] = None
-        #     cloth_links[12] = None
-        #     cloth_links[13] = None
-        #     cloth_links[14] = None
-        #     cloth_links[15] = None
-        #     cloth_links[16] = None
-        #     cloth_links[17] = None
-        #     cloth_links[18] = None
-        #     cloth_links[19] = None
-        #     cloth_links[22] = None
-        #     cloth_links[23] = None
-        #     cloth_links[24] = None
-        #     cloth_links[25] = None
-        #     force_timer = 0
+            
         for link in cloth_links:
             if link is not None:
                 draw_line(link.p1, link.p2, world)
+        #console.SetConsoleTextAttribute(0x000C) 
         console.WriteConsoleOutputCharacter("".join(world),win32console.PyCOORDType(0, 0))
+        #console.WriteConsole("".join(world))
+        
         ms = 1000 * (time.time() - begin_time)
         force_timer += ms
-        win32console.SetConsoleTitle(f"{ms} ms")
+        
+        win32console.SetConsoleTitle(f"{calc + ms} ms")
+
         #win32console.SetConsoleTitle(f"{win32api.GetCursorPos()} ms")
     
 if __name__ == "__main__":
